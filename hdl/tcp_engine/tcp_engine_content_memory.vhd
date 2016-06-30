2f90e5097e26128484486e07410553bd1a68f8de
Mike Field <hamster@snap.net.nz>
2016-06-30 16:55:42 +1200

    Update message to something more fun.

--- hdl/tcp_engine/tcp_engine_content_memory.vhd
+++ hdl/tcp_engine/tcp_engine_content_memory.vhd
@@ -32,10 +32,10 @@ end tcp_engine_content_memory;
 architecture Behavioral of tcp_engine_content_memory is
     type a_mem is array(0 to 15) of std_logic_vector(7 downto 0);
     -- For now, just the characters '0' to '9' and 'A' to 'F'
-    signal mem : a_mem := (x"30", x"31", x"32", x"33",
-                           x"34", x"35", x"36", x"37",
-                           x"38", x"39", x"41", x"42",
-                           x"43", x"44", x"45", x"46");
+    signal mem : a_mem := (x"46", x"50", x"47", x"41",
+                           x"20", x"73", x"61", x"79",
+                           x"73", x"20", x"22", x"48",
+                           x"69", x"22", x"0D", x"0A");
 begin
     process(clk)
         begin
